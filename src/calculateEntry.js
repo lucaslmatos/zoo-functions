70a2307622f9d98d9f7112dbb35b49c31f8250f4
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 19:08:34 -0300

    Commit 07 - Requisito 08 completo.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const objEntrants = {};
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  objEntrants.child = child;
+  objEntrants.adult = adult;
+  objEntrants.senior = senior;
+  return objEntrants;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const obj = countEntrants(entrants);
+  const soma = obj.child * 20.99 + obj.adult * 49.99 + obj.senior * 24.99;
+  return soma;
 };
 
 module.exports = { calculateEntry, countEntrants };
