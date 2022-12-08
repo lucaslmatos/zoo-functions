b51adfc69822be30cb96bf7b05219ee1418a306d
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 18:18:54 -0300

    Commit 05.5 - Requisito 06.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -17,28 +17,33 @@ describe('Testes da função HandlerElephants', () => {
     expect(actual).toStrictEqual(expected);
   });
   it('Quando receber o parâmetro names, deve retornar um array com a relação dos nomes de todos os elefantes ', () => {
-    const expected = ;
+    const expected = ['Ilana','Orval','Bea','Jefferson'];
     const actual = handlerElephants('names');
     expect(actual).toStrictEqual(expected);
   });
   it('Quando receber o parâmetro averageAge, deve retornar a média de idade dos elefantes  ', () => {
-    const expected = ;
+    const expected = 10.5;
     const actual = handlerElephants('averageAge');
-    expect(actual).toStrictEqual(expected);
+    expect(actual).toBeCloseTo(expected);
   });
   it('Quando receber o parâmetro location, deve retornar a localização dos elefantes dentro do Zoológico ', () => {
-    const expected = ;
+    const expected = 'NW';
     const actual = handlerElephants('location');
     expect(actual).toStrictEqual(expected);
   });
   it('Quando receber o parâmetro popularity, deve retornar a a popularidade dos elefantes ', () => {
-    const expected = ;
+    const expected = 5;
     const actual = handlerElephants('popularity');
     expect(actual).toStrictEqual(expected);
   });
-  it('Quando receber o parâmetro count, deve retornar a quantidade de elefantes ', () => {
-    const expected = 4;
-    const actual = handlerElephants('count');
+  it('Quando receber o parâmetro availability, deve retornar um array com a relação de dias em que é possível visitar os elefantes ', () => {
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    const actual = handlerElephants('availability');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber um parâmetro  diferente dos padrões, deve retornar nulo', () => {
+    const expected = null;
+    const actual = handlerElephants('qualquer');
     expect(actual).toStrictEqual(expected);
   });
 });
