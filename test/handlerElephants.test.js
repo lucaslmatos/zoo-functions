167979a9a8e72b601267ce18a3fd736db900f0af
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 18:09:47 -0300

    Commit 05.5 - Requisito 06 parcialmente finalizado

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,44 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Quando não receber um parâmetro, a função deve retornar Undefinied', () => {
+    const expected = undefined;
+    const actual = handlerElephants();
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando o parâmetro recebido não for uma String, a função deve retornar a mensagem "Parâmetro inválido, é necessário uma string".', () => {
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    const actual = handlerElephants(true);
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro count, deve retornar a quantidade de elefantes ', () => {
+    const expected = 4;
+    const actual = handlerElephants('count');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro names, deve retornar um array com a relação dos nomes de todos os elefantes ', () => {
+    const expected = ;
+    const actual = handlerElephants('names');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro averageAge, deve retornar a média de idade dos elefantes  ', () => {
+    const expected = ;
+    const actual = handlerElephants('averageAge');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro location, deve retornar a localização dos elefantes dentro do Zoológico ', () => {
+    const expected = ;
+    const actual = handlerElephants('location');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro popularity, deve retornar a a popularidade dos elefantes ', () => {
+    const expected = ;
+    const actual = handlerElephants('popularity');
+    expect(actual).toStrictEqual(expected);
+  });
+  it('Quando receber o parâmetro count, deve retornar a quantidade de elefantes ', () => {
+    const expected = 4;
+    const actual = handlerElephants('count');
+    expect(actual).toStrictEqual(expected);
+  });
+});
