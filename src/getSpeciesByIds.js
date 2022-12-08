5e3f793028db1773fd01746de68cbbc2bdbc395c
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 15:25:55 -0300

    Commit 01 - Requisito 01.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) return [];
+  const speciesFound = ids.map((animalId) => data.species.find((specie) => specie.id === animalId));
+  return speciesFound;
 };
 
 module.exports = getSpeciesByIds;
