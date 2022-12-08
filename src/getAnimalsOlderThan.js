e0bc1cbd108bc7e0b54bf84a077f65fb7d7b2423
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 15:41:04 -0300

    Commit 02 - Requisito 02.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const searchAnimal = data.species.find((specie) => specie.name === animal);
+  const avaliador = searchAnimal.residents.every((animalresident) => animalresident.age >= age);
+  return avaliador;
 };
 
 module.exports = getAnimalsOlderThan;
