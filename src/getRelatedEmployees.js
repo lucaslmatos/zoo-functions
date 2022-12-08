89681c35cdfd2aebf34950662bc3082ccd2dfac9
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 16:44:08 -0300

    Commit 04 - Requisito 04.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const employeeFound = data.employees.find((employee) => employee.id === id);
+  const verifyId = data.employees.some((employee) => employee.managers.includes(employeeFound.id));
+  return verifyId;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const employeeMgs = data.employees.filter((employee) => employee.managers.includes(managerId));
+    const employees = employeeMgs.map((employee) => `${employee.firstName} ${employee.lastName}`);
+    return employees;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
