b3f4b7ed03cf5362f808c55b581cde7ec564e6f9
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 21:30:31 -0300

    Commit 08 - Requisito 09 completo. depois de muito tempo quebrando a cabeça

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
+const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+const ifMonday = (scheduleTarget) => {
+  const objRetorno = {
+    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  return objRetorno;
+};
+
+const ifDay = (scheduleTarget) => {
+  const officeHour = data.hours[scheduleTarget];
+  const exhibition = data.species.filter((specie) => specie.availability.includes(scheduleTarget));
+  const namesExhibition = exhibition.map((animal) => animal.name);
+  const retorno = { [scheduleTarget]: { officeHour:
+    `Open from ${Object.values(officeHour)[0]}am until ${Object.values(officeHour)[1]}pm`,
+  exhibition: namesExhibition } };
+  return retorno;
+};
+
+const ifAnimal = (scheduleTarget) => {
+  const retorno = data.species.find((animal) => animal.name === scheduleTarget);
+  return retorno.availability;
+};
+
+const ifAny = (scheduleTarget) => {
+  const retorno = {};
+  for (let i = 0; i < days.length; i += 1) {
+    const animalsDay = data.species.filter((animal) => animal.availability.includes(days[i]));
+    const hourDay = data.hours[days[i]];
+    retorno[days[i]] = {
+      exhibition: animalsDay.map((animal) => animal.name),
+      officeHour: `Open from ${Object.values(hourDay)[0]}am until ${Object.values(hourDay)[1]}pm`,
+    };
+  }
+  retorno.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  return retorno;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (data.species.some((animal) => animal.name === scheduleTarget)) {
+    return ifAnimal(scheduleTarget);
+  }
+  if (scheduleTarget === 'Monday') {
+    return ifMonday(scheduleTarget);
+  }
+  if (days.includes(scheduleTarget)) {
+    return ifDay(scheduleTarget);
+  }
+  return ifAny(scheduleTarget);
 };
 
 module.exports = getSchedule;
