39cff0bfcc48bccb7f4f1df05921fa70ec6f5717
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-08 22:16:38 -0300

    Commit 09 - Requisito 10.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employeeResp = data.employees.find((employee) => employee.id === id);
+  const firstAnim = employeeResp.responsibleFor[0];
+  const anim = data.species.find((animal) => animal.id === firstAnim);
+  const animalAges = anim.residents.map((resident) => resident.age);
+  const olderAge = Math.max(...animalAges);
+  const olderAnim = anim.residents.find((resident) => resident.age === olderAge);
+  return Object.values(olderAnim);
 };
 
 module.exports = getOldestFromFirstSpecies;
