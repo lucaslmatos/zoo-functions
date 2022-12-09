c3ae72961e4726b5b81109342f8b57aad093d0a2
Lucas Matos <lucaslmatos18@gmail.com>
2022-12-09 11:28:42 -0300

    Commit 10 - Requisito 11. Com a ajuda do pessoal da turma, consegui resolver alguns problemas que tive nesse requisito.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,42 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getByName = (name) =>
+  employees.find((employee) => employee.firstName === name || employee.lastName === name);
+
+const getById = (id) =>
+  employees.find((employee) => employee.id === id);
+
+const getSpecies = (employee) =>
+  employee.responsibleFor.map((animal) => species.find((specie) => specie.id === animal));
+
+const info = (employee) => {
+  const name = getSpecies(employee).map((specie) => specie.name);
+  const locations = getSpecies(employee).map((specie) => specie.location);
+  const retorno = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: name,
+    locations,
+  };
+  return retorno;
+};
+
+const identify = (identification) => {
+  if (identification.name) {
+    return getByName(identification.name);
+  } return getById(identification.id);
+};
+
+const getEmployeesCoverage = (identification) => {
+  if (identification) {
+    const employee = identify(identification);
+    if (employee === undefined) {
+      throw new Error('Informações inválidas');
+    }
+    return info(employee);
+  }
+  return employees.map((employee) => info(employee));
 };
 
 module.exports = getEmployeesCoverage;
